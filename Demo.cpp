ff68c86a723a1b22a4819c08b0578bddc5128f01
Nhựt Phi <56862164+nhutphi12032001@users.noreply.github.com>
2021-04-15 16:24:13 +0700

    Add 2 class NhanVien & BienChe

--- Demo.cpp
+++ Demo.cpp
@@ -10,3 +10,32 @@ using namespace  std;
 #define MAX_TEN		50
 #define MAX_MASO	10
 #define LCB     1150000
+
+//Class Nhanvien
+class NhanVien
+{	protected: //private:
+		char HoTen[MAX_TEN];
+		char MaSo[MAX_MASO];
+		float Luong;
+	public:
+		NhanVien();
+		virtual void TinhLuong()=0; 
+		void Xuat(); 
+		virtual void Nhap();
+		float getLuong();
+		//...bo sung code
+};
+
+	
+//Class Bien che
+class BienChe: public NhanVien
+{	protected: //private:
+		float HeSoLuong;
+		float HeSoPhuCap;
+	public:
+		BienChe();
+		void TinhLuong();
+		void Nhap();
+		//...bo sung code
+};
+
