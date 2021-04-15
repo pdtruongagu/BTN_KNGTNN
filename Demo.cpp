924acb4f602506da30ab5890ddfddac8ac49c227
pdtruongagu <80301679+pdtruongagu@users.noreply.github.com>
2021-04-15 16:21:06 +0700

    Khai báo thư viện & các hằng

---
+++ Demo.cpp
@@ -0,0 +1,12 @@
+#include <conio.h>
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include<stdlib.h>
+#include <iomanip>
+#include <iostream>
+using namespace  std;
+
+#define MAX_TEN		50
+#define MAX_MASO	10
+#define LCB     1150000
